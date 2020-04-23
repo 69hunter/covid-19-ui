1a7f72c2719f580dbea1c6f2dc2979bfaff09a1b
khunlingtan <kltan69@gmail.com>
2020-04-24 00:14:35 +0800

    add svg file in header

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:covid19ui/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,22 +34,44 @@ class HomeScreen extends StatelessWidget {
           ClipPath(
             clipper: MyClipper(),
             child: Container(
-              height: 350,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFF3383CD),
-                    Color(0xFF11249F),
+                padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+                height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFF3383CD),
+                      Color(0xFF11249F),
+                    ],
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/virus.png'),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset('assets/icons/menu.svg'),
+                    ),
+                    SizedBox(height: 20),
+                    Expanded(
+                      child: Stack(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            'assets/icons/Drcorona.svg',
+                            width: 230,
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.topCenter,
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
-                ),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/virus.png'),
-                ),
-              ),
-            ),
+                )),
           )
         ],
       ),
